77e6f83511fff7416394146054ee8cc8d249306b
Ramil Salihar <ramilsalihar@gmail.com>
2026-04-05 00:42:39 +0600

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: resolve(__dirname, 'src'),
   plugins: [react()],
+  publicDir: resolve(__dirname, 'src/static'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
 })
